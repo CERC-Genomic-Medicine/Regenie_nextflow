f294d6098d87b02d1d0e51830e715dfa46978618
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2022-01-20 15:45:45 -0500

    Error Correction
    Error Correction

--- test_regenie_concat.sh
+++ test_regenie_concat.sh
@@ -2,10 +2,11 @@
 
 phe=$(head $InDir/$PheName -n 1 | tr ' ' '\t' | cut -f $SLURM_ARRAY_TASK_ID)
 
+
 gunzip step2_*_$phe.regenie.gz
-cat step2_1_$phe.regenie > $phe.regenie
+cat $OutDir/step2_1_"$phe".regenie > $OutDir/"$phe".regenie
 
 for ((i=2; i<=$njobs; i++))
 do
-  sed 1d step2_$i_$phe.regenie >> $phe.regenie
+  sed 1d $OutDir/step2_"$i"_"$phe".regenie >> $OutDir/"$phe".regenie
 done
