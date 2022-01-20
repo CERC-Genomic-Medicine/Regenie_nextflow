c83fb26367e73fee39f6c5790dfdd03be3028f07
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2022-01-20 13:48:18 -0500

    workable update
    eliminate first line inserted into final file multiple time

--- test_regenie_concat.sh
+++ test_regenie_concat.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 phe=$(head $InDir/$PheName -n 1 | tr ' ' '\t' | cut -f $SLURM_ARRAY_TASK_ID)
-echo $phe
-echo $SLURM_ARRAY_TASK_ID
+
 gunzip step2_*_$phe.regenie.gz
-cat step2_*_$phe.regenie > $phe.regenie
+cat step2_1_$phe.regenie > $phe.regenie
+
+for ((i=2; i<=$njobs; i++))
+do
+  sed 1d step2_$i_$phe.regenie >> $phe.regenie
+done
