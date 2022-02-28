47c0cc5ea30f155eabb0d8b6e47934d72c465d06
VChapdelaineT <96508889+VChapdelaineT@users.noreply.github.com>
2022-02-28 17:11:40 -0500

    Update dephasing.sh

--- util/Scripts/dephasing.sh
+++ util/Scripts/dephasing.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
-#Use : to test and unphase if necessary vcf file produced during the file generation for step 1
-if grep --quiet '.|.' all.common_independent_snps.vcf ; then sed -i '/^##/! s/|/\//g'  all.common_independent_snps.vcf ; fi
+while getopts f: flag
+do
+    case "${flag}" in
+        f) file=${OPTARG};;
+  esac
+done
+if grep --quiet '.|.' $file ; then sed -i '/^##/! s/|/\//g'  $file ; fi
